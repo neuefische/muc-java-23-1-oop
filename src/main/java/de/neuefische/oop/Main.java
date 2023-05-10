0bffc4a1025d1b9149016a1cf6c46a447de0573d
Sergej Grilborzer <sergej@grilborzer.de>
2023-05-10 15:56:11 +0200

    Create constructor + setter/getter

--- src/main/java/de/neuefische/oop/Main.java
+++ src/main/java/de/neuefische/oop/Main.java
@@ -1,39 +1,63 @@
 package de.neuefische.oop;
 
-import java.util.Scanner;
-
-
+import de.neuefische.oop.car.Car;
 
 public class Main {
 
 
     public static void main(String[] args) {
         // Car = type, Object = bmw
-        Car bmw = new Car();
+        // int a = 1;
+        // Neues Studenten-Objekt erzeugt/ gemacht
+        // <Klassentyp> <variablenName> = new <Klassentyp>();
 
-        bmw.color = "green";
-        bmw.manufacturer = "BMW";
-        bmw.year = 2014;
-        bmw.model = "3 Series";
-        System.out.println(bmw.printCar());
+        Car muslimsMercedes = new Car("black");
+
+        Car dirksVw = new Car("black", "VW", 2006, "Passat");
+
+        // Kommen hier die Werte aus dem Konstruktor raus?
+        System.out.println(dirksVw.printCar());
+
+//        dirksVw.setColor("black");
+//        dirksVw.setManufacturer("VW");
+//        dirksVw.setYear(2006);
+//        dirksVw.setModel("Passat");
 
 
-        Car audi = new Car();
-        audi.color = "red";
-        audi.manufacturer ="Audi";
-        audi.model = "A4";
-        audi.year = 2019;
-        System.out.println(audi.printCar());
 
 
 
-        Car mercedes = new Car();
-        mercedes.color = "black";
-        mercedes.year = 2023;
-        mercedes.manufacturer = "Mercedes";
-        mercedes.model = "C-Class";
 
-        System.out.println(mercedes.printCar());
+
+
+
+
+
+        dirksVw.getColor();
+
+        dirksVw.setEmissionRanking("B");
+
+        dirksVw.setColor("black");
+        dirksVw.setManufacturer("BMW");
+        dirksVw.setYear(2014);
+        dirksVw.setModel("3 Series");
+
+//        System.out.println(dirksVw.printCar());
+//
+//        Car audi = new Car();
+//        audi.color = "red";
+//        audi.manufacturer ="Audi";
+//        audi.model = "A4";
+//        audi.year = 2019;
+//        System.out.println(audi.printCar());
+//
+//        Car mercedes = new Car();
+//        mercedes.color = "black";
+//        mercedes.year = 2023;
+//        mercedes.manufacturer = "Mercedes";
+//        mercedes.model = "C-Class";
+
+//        System.out.println(mercedes.printCar());
 
     }
 }
---
+++ src/main/java/de/neuefische/oop/car/Car.java
@@ -0,0 +1,85 @@
+package de.neuefische.oop.car;
+
+// Klasse = Wo man Objekte definiert
+/*
+* Klasse = Bauplan für Objekte = Neuer Typ
+* - Hat Eigenschaften
+* - Hat Methoden
+*
+* - Sollte eine toString Methode beinhalten
+*
+* */
+public class Car {
+    // Eigenschaften von einer Klasse
+    private String color;
+    private String manufacturer;
+    private int year = 2020;
+    private String model;
+
+    private String emissionRanking;
+
+    public Car(String color) {
+        this.color = color;
+    }
+
+    // Konstruktor = Bauanleitung = Methode die uns ein neues Objekt liefert
+    // public = Welche Sichtbarkeit wir haben
+    // Car = Der Klassenname und gleichzeitig der
+    // Rückgabewert
+    public Car(String color, String manufacturer, int year, String model) {
+        System.out.println("Hallo, I bims der Konstruktor von Car!");
+        System.out.println("ich hab bekommen die werte: " + color + " " + manufacturer);
+
+        this.color = color;
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.model = model;
+    }
+
+    public void setEmissionRanking(String emissionRanking) {
+        // Geben den neuen Wert an die Eigenschaft
+        this.emissionRanking = emissionRanking;
+    }
+
+
+    // Für get:
+    // Neue Methode
+    // get + <Name der Eigenschaft>
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    // Für set:
+    // Neue Methode
+    // set + <Name der Eigenschaft>
+    // Übergeben den neuen Wert
+
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    // Methoden
+    // Protected = Man darauf darauf zugreifen,
+    // wenn man im selben package ist
+    public String printCar(){
+        return  "[Color: " + color + " Manufacturer:  "+ manufacturer + "  Year:  " +year + "  Model:  "+ model+ "]";
+    }
+}
